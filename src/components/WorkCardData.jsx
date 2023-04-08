d8b2f6b214130beeabab5c87a38395f324fd0760
alphardhafiz <bucialphard@gmail.com>
2023-04-08 18:01:24 +0700

    edit link project

--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -5,7 +5,8 @@ const ProjectCardData = [
     img: portoImg,
     title: "Fivenoodle - Landing Page",
     text: "Fivenoodle is a website landing page that offers a food product with the theme of healthy noodles made from high quality organic ingredients and using admin pannel for handle CRUD at menu page and blog page",
-    urlProject: "url.com",
+    urlProject:
+      "https://drive.google.com/file/d/1vn2ITrgTEV0D6ALXYOECXnzkhGjPfVrD/view?usp=share_link",
   },
 ];
 
