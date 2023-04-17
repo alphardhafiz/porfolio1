229903da45d324f85ea1a60326f529ff770b19c1
alphardhafiz <bucialphard@gmail.com>
2023-04-17 12:40:55 +0700

    add project

--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -1,10 +1,18 @@
 import portoImg from "../assets/projectImg1.webp";
+import portoImg2 from "../assets/projectImg2.webp";
 
 const ProjectCardData = [
   {
     img: portoImg,
     title: "Fivenoodle - Landing Page",
     text: "Fivenoodle is a website landing page that offers a food product with the theme of healthy noodles made from high quality organic ingredients and using admin pannel for handle CRUD at menu page and blog page",
+    urlProject:
+      "https://drive.google.com/file/d/1vn2ITrgTEV0D6ALXYOECXnzkhGjPfVrD/view?usp=share_link",
+  },
+  {
+    img: portoImg2,
+    title: "TODO APP",
+    text: "",
     urlProject:
       "https://drive.google.com/file/d/1vn2ITrgTEV0D6ALXYOECXnzkhGjPfVrD/view?usp=share_link",
   },
